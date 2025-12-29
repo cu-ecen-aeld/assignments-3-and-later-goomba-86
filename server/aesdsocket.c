e14114d5dde3102087bc0d6a49b0f7e36a523d67
Olli Sjöberg <olli.a.sjoberg@gmail.com>
2025-12-29 14:26:28 +0200

    first version of multi threaded socket

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,8 +1,10 @@
+#include "queue.h"
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -17,6 +19,25 @@
 static int fd = 0;
 static char const *const stringdata = "/var/tmp/aesdsocketdata";
 
+typedef struct thread_data_t {
+  int clientfd;
+  bool thread_completed;
+  pthread_t threadid;
+} thread_data_t;
+
+typedef struct entry_t {
+  thread_data_t *data;
+  SLIST_ENTRY(entry_t) entries;
+} entry_t;
+
+SLIST_HEAD(listhead, entry_t);
+
+pthread_mutex_t lock;
+
+void write_file_back_to_client(int clientfd);
+void write_bytes_to_file(int clientfd);
+void *handle_thread(void *arg);
+
 void log_info(const char *message) {
   syslog(LOG_INFO, "%s", message);
   printf("%s\n", message);
@@ -24,6 +45,11 @@ void log_info(const char *message) {
 
 void handle_signal(int signal) {
   if (signal == SIGINT || signal == SIGTERM) {
+
+    // TODO: wait for threads to complete
+    /* entry_t *iter; */
+    /* SLIST_FOREACH(iter, &listhead, entries); */
+
     shutdown(fd, SHUT_RDWR);
     log_info("Caught signal, exiting");
     closelog();
@@ -34,8 +60,6 @@ void handle_signal(int signal) {
 
 int main(int argc, char **argv) {
   openlog("aesdsocket", 0, LOG_USER);
-  (void)argc;
-  (void)argv;
   struct addrinfo hints, *res;
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
@@ -86,6 +110,9 @@ int main(int argc, char **argv) {
     return -1;
   }
 
+  struct listhead head;
+  SLIST_INIT(&head);
+
   while (true) {
     struct sockaddr_in clientaddr;
     socklen_t clientaddrsize = sizeof(clientaddr);
@@ -98,43 +125,82 @@ int main(int argc, char **argv) {
     inet_ntop(clientaddr.sin_family, &clientaddr.sin_addr, ipstr,
               INET_ADDRSTRLEN);
     syslog(LOG_INFO, "Accepted connection from %s", ipstr);
-    /* printf("Accepted connection from %s\n", ipstr); */
 
-    int wfd = open(stringdata, O_CREAT | O_WRONLY | O_APPEND,
-                   S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH);
+    // 1. Create thread
+    thread_data_t *new_data = malloc(sizeof(thread_data_t));
+    new_data->clientfd = clientfd;
+    new_data->thread_completed = false;
+    if (pthread_create(&new_data->threadid, NULL, handle_thread, NULL) != 0) {
+      syslog(LOG_ERR, "Thread creation failed.");
+      exit(1);
+    }
 
-    char inbuffer[1024] = {0};
-    int bytesread = 0;
-    while ((bytesread = recv(clientfd, inbuffer, sizeof(inbuffer), 0)) > 0) {
-      ssize_t byteswritten = write(wfd, inbuffer, bytesread);
-      if (byteswritten < 0) {
-        return -1;
+    // 2. Store thread to a linked list
+    entry_t *entry = malloc(sizeof(entry_t));
+    entry->data = new_data;
+    SLIST_INSERT_HEAD(&head, entry, entries);
+
+    // 3. Remove completed threads
+    entry_t *iter, *tmp;
+    SLIST_FOREACH_SAFE(iter, &head, entries, tmp) {
+      if (iter->data->thread_completed) {
+        SLIST_REMOVE(&head, iter, entry_t, entries);
+        pthread_join(iter->data->threadid, NULL);
+        close(iter->data->clientfd);
+        free(iter->data);
+        free(iter);
       }
-      if (inbuffer[bytesread - 1] == '\n') {
-        break;
-      }
-    }
-    close(wfd);
-
-    int rfd = open(stringdata, O_RDONLY);
-
-    if (rfd < 0) {
-      syslog(LOG_ERR, "Failed to open file for reading.");
     }
 
-    char buffer[1024] = {0};
-    ssize_t received;
-    while ((received = read(rfd, buffer, sizeof(buffer))) > 0) {
-      /* printf("Writing buffer back %s\n", buffer); */
-      /* printf("Received bytes: %zi\n", received); */
-      send(clientfd, buffer, received, 0);
-    }
-
-    close(rfd);
-    close(clientfd);
     printf("Closed connection from %s\n", ipstr);
   }
 
   remove(stringdata);
   return 0;
+}
+
+void *handle_thread(void *arg) {
+  thread_data_t *data = (thread_data_t *)arg;
+  pthread_mutex_lock(&lock);
+  write_bytes_to_file(data->clientfd);
+  write_file_back_to_client(data->clientfd);
+  data->thread_completed = true;
+  pthread_mutex_unlock(&lock);
+  return arg;
+}
+
+void write_bytes_to_file(int clientfd) {
+  int wfd = open(stringdata, O_CREAT | O_WRONLY | O_APPEND,
+                 S_IRUSR | S_IRGRP | S_IROTH | S_IWUSR | S_IWGRP | S_IWOTH);
+
+  char inbuffer[1024] = {0};
+  int bytesread = 0;
+  while ((bytesread = recv(clientfd, inbuffer, sizeof(inbuffer), 0)) > 0) {
+    ssize_t byteswritten = write(wfd, inbuffer, bytesread);
+    if (byteswritten < 0) {
+      return;
+    }
+    if (inbuffer[bytesread - 1] == '\n') {
+      break;
+    }
+  }
+  close(wfd);
+}
+
+void write_file_back_to_client(int clientfd) {
+  int rfd = open(stringdata, O_RDONLY);
+
+  if (rfd < 0) {
+    syslog(LOG_ERR, "Failed to open file for reading.");
+  }
+
+  char buffer[1024] = {0};
+  ssize_t received;
+  while ((received = read(rfd, buffer, sizeof(buffer))) > 0) {
+    /* printf("Writing buffer back %s\n", buffer); */
+    /* printf("Received bytes: %zi\n", received); */
+    send(clientfd, buffer, received, 0);
+  }
+
+  close(rfd);
 }
